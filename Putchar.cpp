226196f53b12ae3774a0d111dc03bc00ce3fcbd2
suryapratap2312 <87615712+suryapratap2312@users.noreply.github.com>
2023-03-06 00:00:05 +0530

    Create Putchar.cpp

---
+++ Putchar.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("\n Enter the character: ");
+    ch = getchar();
+    printf("\nThe character is: ");
+    putchar(ch);
+    return(0);
+}
